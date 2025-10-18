b2850ddb6cde39cac81c9ec8fa2e6a63dac375f8
MooraRodriguezz <mora.mardelplata@gmail.com>
2025-10-17 21:16:15 -0300

    Agrego dependencias de Maven asi funciona la BDD

--- src/main/java/com/example/programapapa/HelloApplication.java
+++ src/main/java/com/example/programapapa/HelloApplication.java
@@ -1,16 +1,25 @@
 package com.example.programapapa;
 
+import BDD.OrdenDAO;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
 
+        OrdenDAO ordenDAO = new OrdenDAO();
+
+        ordenDAO.inicializarBaseDeDatos();
+
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("Gestión de Taller");
+        stage.setScene(scene);
+        stage.show();
     }
 }
